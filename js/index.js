b9d46b63977b05e931843f7edb1c37da0f67df82
Nicola Lerra <github.nicolalerra@gmail.com>
2022-09-12 16:27:40 +0200

    prova

---
+++ js/index.js
@@ -0,0 +1,4 @@
+// alert('Usa il pop up per dare dei messaggi');
+// window.alert('Sto studiando JS');
+
+alert('Ciao, sono Siri! Scrivi il numero da calcolare?')
---
+++ js/naming.js
@@ -0,0 +1,29 @@
+//var 1 pet_preferito = 'gatto';
+//console.log(1 pet_preferito);
+
+/*var pet = 'gatto';
+var Pet = 'cane';*/
+// var petPreferito = 'criceto';
+// var PetPreferito = 'coniglio';
+
+// var $pet = 'giraffa';
+// console.log($pet);
+// var _pet = 'leone';
+// console.log(_pet);
+// var _pet2 = 'pantera';
+// document.write(_pet2);
+
+//var 12 = 'numero';
+
+
+
+var primoNumero = Number(prompt("scrivi il primo numero"))
+var secondoNumero = Number(prompt("scrivi il secondo numero"))
+
+document.getElementById("siri-calcolatrice").innerHTML = primoNumero + secondoNumero
+
+document.getElementById("siri-calcolatrice").style.color = 'red'
+
+
+
+
